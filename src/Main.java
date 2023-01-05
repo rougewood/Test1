85dbc655a1ea8a0db2dac81ea48a25df2c471a13
roger <roger.zhangyu@gmail.com>
2023-01-05 09:50:14 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String... args){
+        extracted("", "");
+        test();
+    }
+
+    private static void extracted(String m, String l) {
+        System.out.println("sdfasdf sadf asdf");
+        System.out.println("sdfasdf sadf asdf");
+    }
+
+    public static void test(){}
+}
---
+++ src/com/test/MainTest.java
@@ -0,0 +1,18 @@
+package com.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void test1() {
+    }
+}
